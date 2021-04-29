8981b905828cfac7a5344c7e99008d4b7b54c14e
Michel Nassalang <68905963+Michel-Nassalang@users.noreply.github.com>
2021-04-29 11:45:28 +0000

    Add files via upload

---
+++ js/zoom_effet.js
@@ -0,0 +1,32 @@
+
+    // zoom = 2;
+    // loupe = document.getElementById("loupe");
+    // document.body.onmousemove = function(event){
+    //     loupe.style.left = event.clientX+200;
+    //     loupe.style.top = event.clientY+200;
+    // }
+
+    // loupe = document.getElementById("loupe");
+    // initial = document.getElementById("initial");
+    // var zoom, largeur, hauteur, carre;
+    // function preparer(){
+    //     zoom = 3;
+    //     largeur = 500;
+    //     hauteur = 300;
+    //     carre = 200;
+    //     initial.width = largeur;
+    //     initial.height = hauteur;
+    //     loupe.height = zoom*hauteur;
+    //     loupe.width = zoom*largeur;
+    // }
+    // document.body.onmousemove = function(){
+    //     zoom = 3;
+    //     largeur = 500;
+    //     hauteur = 300;
+    //     carre = 200;
+    //     initial.width = largeur;
+    //     initial.height = hauteur;
+    //     loupe.height = zoom*hauteur;
+    //     loupe.width = zoom*largeur;
+    // }
+    // console.log(loupe);
