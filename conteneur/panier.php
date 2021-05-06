b7a1c50e2c59b15d470bcd7dccd71f9f2e5480ab
Michel Nassalang <68905963+Michel-Nassalang@users.noreply.github.com>
2021-05-06 17:58:22 +0000

    Add files via upload

--- conteneur/panier.php
+++ conteneur/panier.php
@@ -1,4 +1,13 @@
 <?php 
+try {
+    $db = new PDO('mysql:host=localhost;dbname=site-e-commerce','root','');
+    $db ->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER); // les noms des caracteres seront en minuscules
+    $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // les erreurs lanceront des exceptions
+}
+catch(exception $e){
+    echo 'Une erreur est survenue';
+    die();
+}
 require_once('start.php');
 require_once('header.php');
 ?> 
@@ -8,7 +17,54 @@ require_once('header.php');
         <h1>Panier Client</h1>
     </div>
 <?php 
-
+if(isset($_SESSION['pseudo'])){
+    ?>
+    <div class="panier">
+        <div class="tete">
+            <nav>
+                <div class="p one">Image</div>
+                <div class="p two">Titre</div>
+                <div class="p three">Prix</div>
+                <div class="p four">Quantité</div>
+                <div class="p five">Total</div>
+            </nav>
+        </div>
+        <div class="produits">
+            <?php 
+                $select = $db ->prepare('SELECT * FROM panier WHERE client=:client');
+                $select->execute([
+                    "client"=>$_SESSION['pseudo']
+                ]);
+                while ($ligne = $select->fetch(PDO::FETCH_OBJ)) {
+                    $produit = $db->prepare('SELECT * FROM produits WHERE titre=:title');
+                    $produit->execute([
+                        "title"=> $ligne->produits
+                    ]);
+                    while($pro = $produit->fetch(PDO::FETCH_OBJ)){
+                    ?>
+                    <nav>
+                        <div class="p one"><img src="../images_produits/<?= $pro->titre ?>.png" alt=""></div>
+                        <div class="p two"><?= $pro->titre ?></div>
+                        <div class="p three"><?= $pro->prix ?> frcs</div>
+                        <div class="p four"><?= $ligne->quantite ?></div>
+                        <div class="p five">
+                            <?= $ligne->quantite*$pro->prix ?> frcs <br> 
+                            <a href=""><img src="" alt=""> supprimer</a>
+                        </div>
+                    </nav>
+                    <?php
+                    }
+                }
+            ?>  
+            <a class="commande" href="#"> Passer la commande</a> 
+        </div>
+    </div>
+    <?php
+}else {
+    ?>
+    <h1>Désolé, vous n'êtes pas connecté donc vous ne pouvez pas accedez à un panier.</h1>
+    <?php  
+}
 ?> 
 </div>
 
